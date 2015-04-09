0bb5b13bc6db656464fa299c2125c010929c463a
Carolina <cweibring@rallydev.com>
2015-04-08 21:09:47 -0600

    adding files

---
+++ src/sample/HelloWorld.java
@@ -0,0 +1,19 @@
+package sample;
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");          // the old SysO-statement
+    }
+
+    public int add(int a, int b) {
+    	return a+b;
+    }
+
+    public int subtract(int a, int b) {
+    	return a-b;
+    }
+
+
+}
